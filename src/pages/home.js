1aa287319b01dd02b329a43bf07f980a4e67c297
devarathinam <devarathinambdr@gmail.com>
2021-10-04 10:35:40 +0530

    first update

---
+++ src/pages/home.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+import {NavLink} from 'react-router-dom';
+
+class Home extends Component{
+
+  render(){
+    return(
+      <div>
+        <h1>This is a HOME PAGE</h1>
+        <NavLink to="/about-us">About Page</NavLink>
+        <br/>
+        <NavLink to="/login">Login Page</NavLink>
+        <br/>
+        <NavLink to="/product">Products</NavLink>
+        <br/>
+        <NavLink to="/contact">Contact</NavLink>
+        <br/>
+        <NavLink to="/mail">Go to Mail Page  </NavLink>
+      </div>
+    )
+  }
+}
+
+export default Home;  
